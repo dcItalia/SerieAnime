5ce9ce50fae7d77bb2bf3b76abc848e172b3381f
Tizioincognit0 <40031873+Tizioincognit0@users.noreply.github.com>
2020-02-20 15:32:15 +0100

    Update Calcolatore episodi di Detective Conan.py

--- Calcolatore episodi di Detective Conan.py
+++ Calcolatore episodi di Detective Conan.py
@@ -2,5 +2,5 @@ Numero = input("Inserisci il numero dell'episodio: ")
 NumeroEp = str(Numero)
 url = input("Inserusci l'URL dell'episodio (Wstream): ")
 Titolo = input("Inserisci il titolo dell'episodio: ")
-Codice = "if($msg == "+NumeroEp+")\n\n\n\n\n{  $menua = '[[{\"text\": \" Wstream \", \"url\": "+url+"\"\}\]\]';\n\n\n\n\n  $mess = \""+Titolo+"\";\n  itastiera($chatID, $mess, $menua,$ticket);\n{"
+Codice = "if($msg == "+NumeroEp+")\n\n\n\n\n{  $menua = '[[{\"text\": \" Wstream \", \"url\": "+url+"\"\}\]\]';\n\n\n\n\n  $mess = \""+Titolo+"\";\n  itastiera($chatID, $mess, $menua,$ticket);\n}\n{"
 print(Codice)
